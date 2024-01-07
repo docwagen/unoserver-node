1a373cc14e57422eebbe960cbe82d94912b3e79c
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-07 01:08:12 +0000

    update project name

--- src/core/BaseService.js
+++ src/core/BaseService.js
@@ -113,7 +113,7 @@ class BaseService {
     });
 
     subProcess.on("close", (code) => {
-      debug("node-unoserver finished with code: %s", code);
+      debug("unoserver-node finished with code: %s", code);
       if (stderr.length) {
         const error = new Error(Buffer.concat(stderr).toString("utf8"));
         debug("%o", error);
